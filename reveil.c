605afb8f958f9ae54f80b42ef63908d8c444c733
Martin <Martin.cortopassi@isen.yncrea.fr>
2020-12-02 13:55:16 +0100

    Auto stash before revert of "merge"

--- reveil.c
+++ reveil.c
@@ -4,12 +4,25 @@
 #include <signal.h>
 #include <time.h>
 
+<<<<<<< Updated upstream
+=======
+void interrupt()//Gestion de l'interruption
+{
+	printf("Fermerture du coucou ...\n");
+	exit(0);
+}
+
+>>>>>>> Stashed changes
 int main (void)
 {
 	int h, min;
 	int hr, minr;//heure du reveil
 	int son;
+<<<<<<< Updated upstream
 		
+=======
+	signal(SIGUSR1,interrupt);
+>>>>>>> Stashed changes
 	printf("Quelle son voulez vous mettre ?\n");
 	printf("son1\n");
 	printf("son2\n");
